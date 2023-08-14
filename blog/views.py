39e41f809d20d2881add5d112e86c78df657e3ef
Ysungho <ysh01022320@gmail.com>
2023-08-14 09:21:05 +0900

    post 작성 페이지 기본 틀 생성

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category, Tag
 
 
@@ -59,6 +59,11 @@ def tag_page(request, slug):
             'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
 # def index(request):
 #    posts = Post.objects.all().order_by('-pk')
